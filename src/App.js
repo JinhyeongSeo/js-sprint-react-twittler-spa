ff02c4d9c3b977088833f8af00bc2936b904f627
JinhyeongSeo <jh4clover@naver.com>
2024-05-29 11:23:54 +0900

    뒤로가기 만듬

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import './App.css';
 import './global-style.css';
+import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
 // TODO - react-router-dom을 설치 후, import 구문을 이용하여 BrowserRouter, Routes, Route 컴포넌트를 불러오세요.
 
 import Sidebar from './Sidebar';
 import Tweets from './Pages/Tweets';
+import MyPage from "./Pages/MyPage";
+import About  from "./Pages/About";
+import UseNavigate from "./UseNavigate";
 // TODO - import문을 이용하여 MyPage, About 컴포넌트를 불러오세요.
 
-const App = () => {
-  return (
-    <div>
-      {/* TODO - BrowserRouter 컴포넌트를 작성합니다. */}
-      <div className="App">
-        <main>
-          <Sidebar />
-          <section className="features">
-            {/* TODO - Routes와 Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
-            <Tweets />
-          </section>
-        </main>
-      </div>
-    </div>
-  );
-};
+
+    const App = () => {
+        return (
+            <div>
+                {/* TODO - BrowserRouter 컴포넌트를 작성합니다. */}
+                <BrowserRouter>
+                    <div className="App">
+                        <main>
+                            <Sidebar/>
+                            <section className="features">
+                                {/* TODO - Routes와 Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
+                                <Routes>
+                                    {/* 경로는 path로 컴포넌트는 element로 연결합니다.*/}
+                                    <Route path="/" element={<Tweets/>}/>
+                                    <Route path="/about" element={<About/>}/>
+                                    <Route path="/mypage" element={<MyPage/>}/>
+                                </Routes>
+                            </section>
+                        </main>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    };
 
 // ! 아래 코드는 수정하지 않습니다.
 export default App;
